7e8e446c9b519863ea38b2467409172bf0ad5dbc
HasmH <h.hasam@outlook.com>
2022-04-28 22:47:40 +1000

    refactored to work on virtual environment - now the repo shoud be standardized

--- scum_buster/user/views.py
+++ scum_buster/user/views.py
@@ -3,15 +3,5 @@ from django.shortcuts import render
 
 from user.models import TrustFactor
 
-def player(request, player_id):
-    player = TrustFactor.objects.get(player_id=player_id)
-    return HttpResponse("Player: %s." % player.player_id)
-
-def reports(request, player_id):
-    player = TrustFactor.objects.get(player_id=player_id)
-    reports = "Reported this many times: %s."
-    return HttpResponse(reports % player.number_of_reports)
-
-def trust(request, player_id):
-    player = TrustFactor.objects.get(player_id=player_id)
-    return HttpResponse("Are they trustworthy? %s." % player.trust_factor)
+def player(request):
+    return HttpResponse("Hey Scumbag")
